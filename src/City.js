a68a227f2e39253320bc718407d3a93e6d58f13b
15219473040 <2362999724@qq.com>
2018-03-17 23:53:51 +0800

    稍作修改，后期继续专门优化

--- src/City.js
+++ src/City.js
@@ -24,23 +24,20 @@ export default class City extends Component{
         var tarName = tar.name
        
         if (tarName=='pro'){
-            // this.setState({ list_pro: this.state.list_pro[tar.value]})
-            // this.setState({ list_city: this.getArray(tarName,"0_", tar.value)})
-            // this.setState({valP:this.state.list_pro[tar.value]})
-            this.setState({ valP: this.state.list_pro[tar.value] })
+
             this.setState({ list_city: this.getArray(tarName, "0_", tar.value) })
-            console.log(tar)
+            this.setState({ list_area: ["区"] })
+            this.setPCA(this.state.list_pro[tar.value], "市", "区")
         }
         if(tarName=="city"){
-            console.log(this.pId + tar.value)
-            this.setState({ valC: this.state.list_city[tar.value] })
             var areaArr=this.getArray(tarName,this.pId+"_",tar.value )
             this.setState({list_area:areaArr})
+            this.setPCA(this.state.valP, this.state.list_city[tar.value] , areaArr[0])
         }
         if(tarName=='area'){
             this.setState({ valA: this.state.list_area[tar.value] })
         }
-        // console.log(this.refs[name].props.val)
+    
     } 
     getArray(tar,fir,sec){
         var id=fir+sec
@@ -50,15 +47,22 @@ export default class City extends Component{
         if(tar=="city"){
             this.cId=id;
         }
-        console.log(id, this.state.all[id])
+    
         return this.state.all[id]
+    }
+    setPCA(p,c,a){
+        this.setState({
+            valP:p,
+            valC:c,
+            valA:a
+        })
     }
  
     render(){
         return (<div> 
-            <MySelect name="pro" ref="pro" list={this.state.list_pro||[]} onchangeHandle={this.check.bind(this)} val={this.state.valP} />
-            <MySelect name="city" ref="city" list={this.state.list_city || []} onchangeHandle={this.check.bind(this)} val={this.state.valC} />
-            <MySelect name="area" ref="area" list={this.state.list_area || []} onchangeHandle={this.check.bind(this)} val={this.state.valA} />
+            <MySelect name="pro"   list={this.state.list_pro||[]} onchangeHandle={this.check.bind(this)} val={this.state.valP} />
+            <MySelect name="city"   list={this.state.list_city || []} onchangeHandle={this.check.bind(this)} val={this.state.valC} />
+            <MySelect name="area"   list={this.state.list_area || []} onchangeHandle={this.check.bind(this)} val={this.state.valA} />
         </div>)
     }
 }
